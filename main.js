d31782e485d51a89a4eb3da6841e79f65bcd5746
MatthewLeeHan <mlh6fc@virginia.edu>
2017-06-09 10:22:24 -0400

    added auto scroll functionality from button Curriculum in navbar

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 function scrollToTop(){
     var topOfDiv = $("#aboutPage").offset().top;
     console.log(topOfDiv);
-    $('body').animate({scrollTop: topOfDiv}, 1000);
+    $('body').animate({scrollTop: topOfDiv}, 1300);
 }
+function scrollToTopTwo(){
+    var topOfDiv = $("#skillsSection").offset().top;
+    console.log(topOfDiv);
+    $('body').animate({scrollTop: topOfDiv}, 1500);
+}
+//function scrollToTopThree(){
+//    var topOfDiv = $("#teachers").offset().top;
+//    console.log(topOfDiv);
+//    $('body').animate({scrollTop: topOfDiv}, 1500);
+//}
